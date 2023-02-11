76a7abfec37c4005bf3d0bac5a70c232b8eec035
M-WRI <me@mwri.de>
2023-02-11 14:53:36 +0100

    my notes from the second day

---
+++ 2_day/day_2_notes.py
@@ -0,0 +1,21 @@
+print(type(num_char), " - this is your type")
+
+# Type conversion
+#string_num_char = str(num_char)
+#print(string_num_char, " - this is your type")
+
+# Rounding numbers
+print(round(8 / 3))
+print(round(8 / 3, 2))
+
+# More methematical operations
+result = 4 / 2
+result /= 2
+print(result)
+
+# F strings
+score = 0
+height = 1.8
+is_winning = True
+
+print(f"your score is {score}\nyour height is {height}\nhave you won? {is_winning}")

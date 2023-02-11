34f12ada9ea12aecdfe554cfafa0211532329b1a
M-WRI <me@mwri.de>
2023-02-11 13:03:38 +0100

    learned how to use variables

---
+++ 1_day/exercise_4_variables.py
@@ -0,0 +1,21 @@
+# Use the inputs of a and b and make a = b and b = a
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+valueOne = a
+valueTwo = b
+a = valueTwo
+b = valueOne
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)

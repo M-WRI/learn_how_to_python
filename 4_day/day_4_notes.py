4cadb8d1e5f3cecf91648b3d609d62275a399217
M-WRI <me@mwri.de>
2023-02-12 16:25:11 +0100

    exercise 1 random 'heads or tails'

---
+++ 4_day/day_4_notes.py
@@ -0,0 +1,15 @@
+# Random numbers
+import random
+import some_file
+
+random_number = random.randint(1, 100)
+
+print(random_number)
+print(some_file.number, "<--- importet number from other file")
+
+random_float = random.random() # Random Float
+print(random_float)
+
+random_float_zero_five = random.uniform(0, 5) # Random Float in range
+print(random_float_zero_five)
+

55eddff7421fe3bd1d367a04f2ad5e974a4142fc
M-WRI <me@mwri.de>
2023-02-16 21:00:12 +0100

    step 4 done

--- 7_day/day_7_final_project_hang_man.py
+++ 7_day/day_7_final_project_hang_man.py
@@ -1,20 +1,34 @@
 import random
 
 # Step 1
-guess = input("guess a letter: ").lower()
 word_list = ["ardvark", "baboon", "camel"]
+
 # random_index = random.randint(0, len(word_list) - 1) <-- My appriach
 # random_word = word_list[random_index]
 
 random_word = random.choice(word_list)
+print(f"the random word is {random_word}...")
+
 # random_word_chars = [*random_word] <-- for loop is automatically going through all the letters
 # no need to create a list before. anyhow, nice to know how to create easy a list.
 
-for letter in random_word:
-   if letter == guess:
-    print("right")
-   else:
-     print("wrong")
+display = []
+for _ in random_word:
+  display += '_'
+
+print(display)
+
+end_of_game = False
+while not end_of_game:
+   guess = input("guess a letter: ").lower()
+   for i in range(len(random_word)):
+    if random_word[i] == guess:
+        display[i] = guess
+        
+    print(display)
+    if '_' not in display:
+       end_of_game = True
+
 
 
 

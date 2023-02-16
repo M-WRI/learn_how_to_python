25ac428e5e2542e5bdcec546ac723455cbfa5876
M-WRI <me@mwri.de>
2023-02-16 19:52:37 +0100

    learned about while loop and creating functions

---
+++ 6_day/day_5_notes.py
@@ -0,0 +1,17 @@
+# Functions
+def my_function():
+    print("Hello")
+    print("World")
+
+my_function()
+
+# While loop
+def jump():
+    print("i am jumping 1 time")
+
+number_of_hurdles = 6
+
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1
+
